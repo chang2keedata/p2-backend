90b245af8a2ec9c5d0feadbbf34c4f6f72a328ea
DK <jackloveseaweed@gmail.com>
2022-07-06 16:54:51 +0800

    added cors

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const express = require('express');
-const {connect} = require('./MongoUtil');
+const MongoUtil = require('./MongoUtil');
+const cors = require('cors');
 
 const dotenv = require('dotenv').config();
 
 const app = express();
 
-app.use(express.urlencoded({
-    extended: false
-}));
+// app.use(express.urlencoded({
+//     extended: false
+// }));
+
+app.use(cors());
+app.use(express.json());
 
 const MONGO_URI = process.env.MONGO_URI;
 
-app.use(express.urlencoded({
-    extended: false
-}));
-
 async function main() {
-  
     const db = await MongoUtil.connect(MONGO_URI, "project2");
-    app.get('/test', async function(req,res) {
+    console.log('connected to database');
+    app.get('/', async function(req,res) {
         let data = await db.collection('outdoor_activity').find({}).toArray();
         res.send(data);
     });
+    // app.get('/', function(req,res){
+    //     res.send('hello world')
+    // })
+
+    app.listen(3000, function () {
+        console.log("server started");
+    })
 }
 main();
 
-app.listen(3000, function () {
-    console.log("hello world");
-})
