cd53970c613615c1b20491b3c887918d9e90ffcc
DK <jack.chang@trentglobal.edu.sg>
2022-07-06 15:45:27 +0000

    added post

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const MONGO_URI = process.env.MONGO_URI;
 async function main() {
     const db = await MongoUtil.connect(MONGO_URI, "project2");
     console.log('connected to database');
-    app.get('/', async function(req,res) {
+    app.get('/test', async function(req,res) {
         let data = await db.collection('outdoor_activity').find({}).toArray();
         res.send(data);
     });
@@ -26,9 +26,24 @@ async function main() {
     //     res.send('hello world')
     // })
 
-    app.listen(3000, function () {
-        console.log("server started");
+    app.post('/tags', async function(req,res){
+        // TODO: validation (as an execrise for the student)
+        let category = req.body.category;
+        let activity = req.body.activity;
+        // when new Date() is called without an argument, then automatically
+        // it will be the server's date and time
+        // let datetime = req.body.datetime ? new Date(req.body.datetime) : new Date();
+        let result = await db.collection('outdoor_activity').insertOne({
+            'category': category,
+            'activity': activity,
+            // 'datetime': datetime
+        })
+        res.status(201); // set the status code to be 201
+        res.send(result);
     })
 }
 main();
 
+app.listen(3000, function () {
+    console.log("server started");
+})
